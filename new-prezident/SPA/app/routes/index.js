700ed752d42895e8d516567891f9bf666514445c
wsbaser <wsbaser@gmail.com>
2018-05-19 19:58:25 +0300

    do not use long cites for mobiles

--- new-prezident/SPA/app/routes/index.js
+++ new-prezident/SPA/app/routes/index.js
@@ -4,6 +4,11 @@ export default Route.extend({
 	citesHistory: Ember.inject.service(),
 	model(){
 		let allCites = this.get('store').peekAll('cite');
+
+		if(window.isMobile){
+			allCites = allCites.filter(c=>c.get('text').length<100);
+		}
+
 		let watchedCites = this.get('citesHistory').get();
 		let notWatched = allCites.reject(function(item){
 			return watchedCites.includes(item.id);
